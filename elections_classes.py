86790361f4029af65f630a4b4f0ff698e8afe795
Duncan Gray <duncanjdgray@gmail.com>
2021-09-10 17:22:33 +0100

    add remain-leave axis to classes and maps

--- elections_classes.py
+++ elections_classes.py
@@ -127,16 +127,25 @@ class LocalAuthority(Area):
 
 # %% Actor-based classes
 class Actor:
-    def __init__(self, name, lib_auth=0, left_right=0):
+    def __init__(self, name, lib_auth=0, left_right=0, rem_leave=0):
+        self.name = name
         self.lib_auth = lib_auth
         self.left_right = left_right
-        self.name = name
+        self.rem_leave = rem_leave
 
     def __str__(self):
-        return """Name: {name}, Lib-Auth: {lib_auth}, Left-Right: {left_right}""".format(name=self.name, lib_auth=self.lib_auth, left_right=self.left_right)
+        return """Name: {name}, Lib-Auth: {lib_auth}, Left-Right: {left_right}, Remain-Leave: {rem_leave}""".format(
+            name=self.name, 
+            lib_auth=self.lib_auth, 
+            left_right=self.left_right,
+            rem_leave=self.rem_leave)
 
     def __repr__(self):
-        return 'Actor(\'{name}\', {lib_auth}, {left_right})'.format(name=self.name, lib_auth=self.lib_auth, left_right=self.left_right)
+        return 'Actor(\'{name}\', {lib_auth}, {left_right})'.format(
+            name=self.name, 
+            lib_auth=self.lib_auth, 
+            left_right=self.left_right,
+            rem_leave=self.rem_leave)
 
     @property
     def lib_auth(self):
@@ -160,16 +169,38 @@ class Actor:
         else:
             raise ValueError("left_right must be between -10 and 10 inclusive!")
 
+    @property
+    def rem_leave(self):
+        return self._rem_leave
+    
+    @rem_leave.setter
+    def rem_leave(self, rem_leave):
+        if rem_leave >= -10 and rem_leave <= 10:
+            self._rem_leave = rem_leave
+        else:
+            raise ValueError("rem_leave must be between -10 and 10 inclusive!")
+
 class Party(Actor):
-    def __init__(self, name, lib_auth=0, left_right=0, voteshare=0):
-        Actor.__init__(self, name, lib_auth, left_right)
+    def __init__(self, name, lib_auth=0, left_right=0, rem_leave=0, voteshare=0):
+        Actor.__init__(self, name, lib_auth, left_right, rem_leave)
         self.voteshare = voteshare
 
     def __str__(self):
-        return """Name: {name}, Lib-Auth: {lib_auth}, Left-Right: {left_right}, Vote share: {voteshare}%""".format(name=self.name, lib_auth=self.lib_auth, left_right=self.left_right, voteshare=self.voteshare*100)
+        return """Name: {name}, Lib-Auth: {lib_auth}, Left-Right: {left_right}, \
+            Remain-Leave: {rem_leave}, Vote share: {voteshare}%""".format(
+                name=self.name, 
+                lib_auth=self.lib_auth, 
+                left_right=self.left_right, 
+                rem_leave = self.rem_leave, 
+                voteshare=self.voteshare*100)
 
     def __repr__(self):
-        return 'Party(\'{name}\', {lib_auth}, {left_right}, {voteshare})'.format(name=self.name, lib_auth=self.lib_auth, left_right=self.left_right, voteshare=self.voteshare)
+        return 'Party(\'{name}\', {lib_auth}, {left_right}, {rem_leave}, {voteshare})'.format(
+            name=self.name, 
+            lib_auth=self.lib_auth, 
+            left_right=self.left_right, 
+            rem_leave=self.rem_leave, 
+            voteshare=self.voteshare)
 
     @property
     def voteshare(self):
@@ -187,18 +218,28 @@ class Voter(Actor):
         priority_axis will force voter to only care about one axis, not both.
         -1 for lib_auth, 1 for left_right, 0 for both
     """
-    def __init__(self, name, lib_auth=0, left_right=0, priority_axis=0):
-        Actor.__init__(self, name, lib_auth, left_right)
+    def __init__(self, name, lib_auth=0, left_right=0, rem_leave=0, priority_axis=0):
+        Actor.__init__(self, name, lib_auth, left_right, rem_leave)
         self.priority_axis = priority_axis
         self.parties_dist = []
         self.ordered_parties = []
         self.vote_cast = []
 
     def __str__(self):
-        return """Name: {name}, Lib-Auth: {lib_auth}, Left-Right: {left_right}, Priority: {priority_axis}""".format(name=self.name, lib_auth=self.lib_auth, left_right=self.left_right, priority_axis=map_priority_axes[self.priority_axis])
+        return """Name: {name}, Lib-Auth: {lib_auth}, Left-Right: {left_right}, Remain-Leave: {rem_leave}, Priority: {priority_axis}""".format(
+            name=self.name, 
+            lib_auth=self.lib_auth, 
+            left_right=self.left_right,
+            rem_leave = self.rem_leave, 
+            priority_axis=map_priority_axes[self.priority_axis])
 
     def __repr__(self):
-        return 'Actor(\'{name}\', {lib_auth}, {left_right}, {priority_axis})'.format(name=self.name, lib_auth=self.lib_auth, left_right=self.left_right, priority_axis=self.priority_axis)
+        return 'Actor(\'{name}\', {lib_auth}, {left_right}, {rem_leave}, {priority_axis})'.format(
+            name=self.name, 
+            lib_auth=self.lib_auth, 
+            left_right=self.left_right, 
+            rem_leave=self.rem_leave,
+            priority_axis=self.priority_axis)
 
     @property
     def priority_axis(self):
@@ -206,11 +247,11 @@ class Voter(Actor):
 
     @priority_axis.setter
     def priority_axis(self, priority_axis):
-        if priority_axis == -1 or priority_axis == 0 or priority_axis == 1:
+        if priority_axis in map_priority_axes:
             self._priority_axis = priority_axis
             self.priority_axis_readable = map_priority_axes[priority_axis]
         else:
-            raise ValueError("priority_axis can only be -1, 0 or 1")
+            raise ValueError("priority_axis can only have values from " + map_priority_axes.keys())
 
     def gen_parties_dist(self, list_parties):
         self.parties_dist = []            # Empty existing list
@@ -220,15 +261,21 @@ class Voter(Actor):
                     ((party).name, \
                     math.sqrt( \
                         (self.lib_auth - (party).lib_auth)**2 + 
-                        (self.left_right - (party).left_right)**2)))
-            elif self.priority_axis == -1:
-                self.parties_dist.append( \
-                    ((party).name, \
-                    abs(self.lib_auth - (party).lib_auth)))
+                        (self.left_right - (party).left_right)**2 +
+                        (self.rem_leave - (party).rem_leave)**2)))
             elif self.priority_axis == 1:
                 self.parties_dist.append( \
                     ((party).name, \
                     abs(self.left_right - (party).left_right)))
+            elif self.priority_axis == 2:
+                self.parties_dist.append( \
+                    ((party).name, \
+                    abs(self.lib_auth - (party).lib_auth)))
+            elif self.priority_axis == 3:
+                self.parties_dist.append( \
+                    ((party).name, \
+                    abs(self.rem_leave - (party).rem_leave)))
+            
 
     def order_parties(self):
         self.ordered_parties = sorted(self.parties_dist, key=lambda tup: tup[1])
